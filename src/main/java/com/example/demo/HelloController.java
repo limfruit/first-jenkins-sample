465c522f54dcef99c80a9b53d5d15a848620bb04
limfruit <148676716+limfruit@users.noreply.github.com>
2024-03-05 16:31:26 +0900

    update-deploy

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello Jenkins! I'm LIM";
+        return "Hello Jenkins! I'm LIM!!!!!! HAHA";
     }
 }
